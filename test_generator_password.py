a67bb7a67dd043bd79cd3130a0404dbb68906078
Gabriel Navas <gabnanavas.dev@gmail.com>
2023-02-27 16:57:24 -0300

    refactor tests with classes

--- test_generator_password.py
+++ test_generator_password.py
@@ -20,7 +20,7 @@ from generator_password import make_one_special_char, make_one_uppercase_letter,
     make_one_number
 
 
-class TestPasswordGenerator(unittest.TestCase):
+class TestMakeOneSpecialChar(unittest.TestCase):
     def test_make_one_special_char_multiple_ranges(self):
         regex = re.compile(r'^[ -\/:-@\[-`{-~]$')
 
@@ -29,6 +29,8 @@ class TestPasswordGenerator(unittest.TestCase):
                 result = make_one_special_char()
                 self.assertRegex(result, regex)
 
+
+class TestMakeOneUppercaseLetter(unittest.TestCase):
     def test_make_one_uppercase_letter_range_65_90(self):
         regex = re.compile(r'^[A-Z]$')
 
@@ -37,6 +39,8 @@ class TestPasswordGenerator(unittest.TestCase):
                 result = make_one_uppercase_letter()
                 self.assertRegex(result, regex)
 
+
+class TestMakeOneLowercaseLetter(unittest.TestCase):
     def test_make_one_lowercase_letter_range_65_90(self):
         regex = re.compile(r'^[a-z]$')
 
@@ -45,6 +49,8 @@ class TestPasswordGenerator(unittest.TestCase):
                 result = make_one_lowercase_letter()
                 self.assertRegex(result, regex)
 
+
+class TestMakeOneNumber(unittest.TestCase):
     def test_make_one_number_range_ascii_48_57(self):
         ord_range = list(range(48, 58))
 
