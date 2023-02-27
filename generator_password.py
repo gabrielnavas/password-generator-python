0a863e93aa917963e1d62abd0ef6e1d3db1edaa5
Gabriel Navas <gabnanavas.dev@gmail.com>
2023-02-27 16:44:23 -0300

    format file

--- generator_password.py
+++ generator_password.py
@@ -1,5 +1,6 @@
 from random import randint, choice
 
+
 def make_one_special_char():
     # 32-47 58-64 91-96 123-126
     ranges = [
@@ -11,13 +12,16 @@ def make_one_special_char():
     ascii_int = choice(ranges)
     return chr(ascii_int)
 
+
 def make_one_uppercase_letter():
     ascii_int = randint(65, 90)
     return chr(ascii_int)
 
+
 def make_one_lowercase_letter():
     ascii_int = randint(97, 122)
     return chr(ascii_int)
+
 
 def make_one_number():
     number = randint(97, 122)
--- test_generator_password.py
+++ test_generator_password.py
@@ -16,7 +16,8 @@ Funções
 
 import unittest
 import re
-from generator_password import make_one_special_char, make_one_uppercase_letter, make_one_lowercase_letter, make_one_number
+from generator_password import make_one_special_char, make_one_uppercase_letter, make_one_lowercase_letter, \
+    make_one_number
 
 
 class TestPasswordGenerator(unittest.TestCase):
@@ -43,6 +44,7 @@ class TestPasswordGenerator(unittest.TestCase):
             with self.subTest(i=i):
                 result = make_one_lowercase_letter()
                 self.assertRegex(result, regex)
+
     def test_make_one_number_range_ascii_97_122(self):
         ord_range = list(range(97, 123))
 
@@ -50,6 +52,7 @@ class TestPasswordGenerator(unittest.TestCase):
             with self.subTest(i=i):
                 result = make_one_number()
                 self.assertIn(result, ord_range)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
